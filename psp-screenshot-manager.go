4e1ec0b330c474b102566e39609f361fdf1d5528
Lynne <thecoolone3@gmail.com>
2018-08-22 18:59:07 +1000

    Command line options, renamed file

--- psp-screenshot-manager.go
+++ psp-screenshot-manager.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"encoding/csv"
 	"fmt"
+	"github.com/jessevdk/go-flags"
 	"io"
 	"log"
 	"os"
@@ -46,6 +47,24 @@ func loadGames() []Game {
 }
 
 func main() {
+
+	var opts struct {
+		overwrite bool `short:"o" long:"overwrite" description:"Overwrite existing screenshots of the same name. If not provided, will rename files to avoid overwriting."`
+
+		Filepath struct {
+			PathToMe string //the first argument will be the name of this file
+			Filename string
+			Trailing []string
+		} `positional-args:"yes" required:"yes"`
+	}
+
+	_, err := flags.ParseArgs(&opts, os.Args)
+	if err != nil {
+		panic(err)
+	}
+
 	gameDB := loadGames()
-	fmt.Println(gameDB)
+
+	gameDB = gameDB
+	fmt.Println(opts.Filepath.Filename)
 }
