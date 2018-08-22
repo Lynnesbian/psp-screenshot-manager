725161ae9f068a8849f099779d906628fa0386aa
Lynne <thecoolone3@gmail.com>
2018-08-22 17:17:31 +1000

    Initial commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"io"
+	"os"
+)
+
+type game struct {
+	serial    string
+	name      string
+	shortname string
+	category  string
+}
+
+func main() {
+	games, err := 0, 0
+}
